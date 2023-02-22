c7a327d9c8e000c752d45527a0c12e1e4168b08a
“dan4y90” <josephef001@gmail.com>
2023-02-21 23:03:09 -0600

    Updates

---
+++ precache-manifest.a5932bd3cb87afc4bcfd21e5bc9860f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38019b39c90f7e1bb8cc",
+    "url": "/APPTodo_List/static/css/main.ffe90759.chunk.css"
+  },
+  {
+    "revision": "38019b39c90f7e1bb8cc",
+    "url": "/APPTodo_List/static/js/main.38019b39.chunk.js"
+  },
+  {
+    "revision": "1daa8b14f4ed358548f7",
+    "url": "/APPTodo_List/static/js/1.1daa8b14.chunk.js"
+  },
+  {
+    "revision": "a32d1139f6e3be7ee8fc",
+    "url": "/APPTodo_List/static/js/runtime~main.a32d1139.js"
+  },
+  {
+    "revision": "d79c533f2eb7cd569b0675e430dd8267",
+    "url": "/APPTodo_List/index.html"
+  }
+];
